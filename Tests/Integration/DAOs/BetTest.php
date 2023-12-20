b6d8e2d86eacef4e005792033250e6c0bac3cc85
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2023-12-19 22:56:38 -0300

    test/bet/DAO/integration: insert and select many bets

--- Tests/Integration/DAOs/BetTest.php
+++ Tests/Integration/DAOs/BetTest.php
@@ -11,6 +11,7 @@ use PHPUnit\Framework\TestCase;
 class BetTest extends TestCase
 {
     private static $db;
+    private DAOsBet $dao;
 
     public static function setUpBeforeClass(): void
     {
@@ -25,6 +26,16 @@ class BetTest extends TestCase
         self::$db->getConnection()->rollBack();
     }
 
+    protected function setUp(): void
+    {
+        $this->dao = new DAOsBet(self::$db);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->dao->deleteAll();
+    }
+
     public static function provideBet(): array
     {
         $bet = new Bet(15.5, 1.2);
@@ -34,16 +45,45 @@ class BetTest extends TestCase
         ];
     }
 
+    public static function provideBets(): array
+    {
+        $bets = [];
+
+        for ($i=0; $i < rand(0, 10); $i++) { 
+            $bets[] = new Bet((rand(100, 1000) / 100), (rand(100, 1000) / 100));
+        }
+
+        return [
+            'many bets' => [$bets],
+        ];
+    }
+
     #[DataProvider('provideBet')]
     public function testInsertAndGetBet(Bet $bet)
     {
-        $dao = new DAOsBet(self::$db);
-        $dao->insert($bet);
+        $this->dao->insert($bet);
 
-        $bets = $dao->getAll();
+        $bets = $this->dao->getAll();
         $this->assertCount(1, $bets);
         $this->assertSame($bets[0]->getValue(), $bet->getValue());
         $this->assertSame($bets[0]->getResult(), $bet->getResult());
         $this->assertSame($bets[0]->getMultiplier(), $bet->getMultiplier());
+    }
+
+    /** @param Bet[] $bets */
+    #[DataProvider('provideBets')]
+    public function testInsertAndGetManyBets(array $bets)
+    {
+        foreach ($bets as $bet) {
+            $this->dao->insert($bet);
+        }
+
+        $result = $this->dao->getAll();
+        $this->assertCount(count($bets), $result);
+        
+        for ($i=0; $i < count($bets); $i++) { 
+            $this->assertSame($bets[$i]->getValue(), $result[$i]->getValue());
+            $this->assertSame($bets[$i]->getMultiplier(), $result[$i]->getMultiplier());
+        }
     }
 }
