3b1a162d31ef7eb7386ba321a7501dec1ab2339b
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2023-12-19 22:02:16 -0300

    feature/analyzer/service: update winner bet

--- app/Services/AnalyzeResultsService.php
+++ app/Services/AnalyzeResultsService.php
@@ -2,13 +2,18 @@
 
 namespace App\Services;
 
+use App\DAOs\Bet as DAOsBet;
 use Exception;
 use App\Models\Bet;
 use BadMethodCallException;
 
 class AnalyzeResultsService
 {
-    public function __construct(private Bet $bet, private array $games) {
+    public function __construct(
+        private Bet $bet,
+        private array $games,
+        private DAOsBet $dao
+    ) {
         $this->validateInput();
     }
 
@@ -23,6 +28,7 @@ class AnalyzeResultsService
     {
         if ($this->countBetWinningGames() === count($this->bet->getGames())) {
             $this->bet->isWinner();
+            $this->dao->update($this->bet);
         }
 
         return $this->bet;
